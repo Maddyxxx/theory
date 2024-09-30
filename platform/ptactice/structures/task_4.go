71f10fe35ff90897de1031886cb2bed8f204f41c
Maddyxxx <sevo260@gmail.com>
2024-09-30 12:33:14 +0300

    task_4

---
+++ platform/ptactice/structures/task_4.go
@@ -0,0 +1,25 @@
+//Задача 4
+//Что выведет код и почему?
+
+package main
+
+import "fmt"
+
+type MyError struct{}
+
+func (MyError) Error() string {
+	return "MyError!"
+}
+
+func errorHandler(err error) {
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
+
+func main() {
+	var err *MyError
+	errorHandler(err) // Error: <nil>. - Переменная err не инициализирована, то есть равна nil, 
+	err = &MyError{}
+	errorHandler(err) // Error: MyError! - здесь произошел высов функции Error(). Здесь err != nil, поэтому происходит вызов метода Error()
+}
