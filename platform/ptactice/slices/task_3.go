780ac6c9a72a5b80707f6e37f6632e56919efde7
Maddyxxx <sevo260@gmail.com>
2024-09-30 14:04:02 +0300

    slices/task_3

--- platform/ptactice/slices/task_3.go
+++ platform/ptactice/slices/task_3.go
@@ -17,11 +17,11 @@ func main() {
 	fmt.Println(c) // [A B TT E] тк с содержит слайс b = [B, TT]
     fmt.Println(b) // [B TT]
 
-	b = append(b, "HH") // к b добавляется ТТ, b = [B, TT]
+	b = append(b, "HH") // к b добавляется HH, b = [B, TT, HH]
 	fmt.Println(c) // [A B TT HH]
     fmt.Println(b) // [B TT HH]
 
-	b = append(b, "PP") // к b добавляется ТТ, b = [B, TT]
+	b = append(b, "PP") // к b добавляется PP, b = [B, TT, HH, PP]
 	fmt.Println(c) // [A B TT HH], капа с остается 4
     fmt.Println(b) // [B TT HH PP]
 
