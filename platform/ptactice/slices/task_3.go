2e229fbdc8b0beae8200682887566ce111cb32ac
Maddyxxx <sevo260@gmail.com>
2024-09-30 13:16:32 +0300

    slices/task_3

---
+++ platform/ptactice/slices/task_3.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Задача 3
+//Что выведется?
+
+func main() {
+    c := []string{"A", "B", "D", "E"}
+	b := c[1:2] // b присвается значение слайса с, b = [B]
+	fmt.Println(c) // [A B D E]
+    fmt.Println(b) // [B]
+
+    b = append(b, "TT") // к b добавляется ТТ, b = [B, TT]
+	fmt.Println(c) // [A B TT E] тк с содержит слайс b = [B, TT]
+    fmt.Println(b) // [B TT]
+
+	b = append(b, "HH") // к b добавляется ТТ, b = [B, TT]
+	fmt.Println(c) // [A B TT HH]
+    fmt.Println(b) // [B TT HH]
+
+	b = append(b, "PP") // к b добавляется ТТ, b = [B, TT]
+	fmt.Println(c) // [A B TT HH], капа с остается 4
+    fmt.Println(b) // [B TT HH PP]
+
+}
