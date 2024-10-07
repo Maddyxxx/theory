44b125b314622b673edb7c8f8894557a2c722fbc
Maddyxxx <sevo260@gmail.com>
2024-10-07 20:38:03 +0300

    parallelizm/task_6

--- main.go
+++ main.go
@@ -6,5 +6,5 @@ import (
 
 
 func main(){
-	parallelizm.T5()
+	parallelizm.T6()
 }
---
+++ platform/practice/parallelizm/task_6.go
@@ -0,0 +1,45 @@
+package parallelizm
+
+import (
+	"fmt"
+	"sync"
+)
+
+//Задача 6
+// Что выведет код и как исправить?
+
+// данный код выведет мапу globalMap с ключами test, test2 и test3, содержащими рандомные значения а (10, 11, 12)
+// в зависимости от порядка выполнения горутин
+// исправить можно
+
+
+
+var globalMap = map[string][]int{"test": make([]int, 0), "test2": make([]int, 0), "test3": make([]int, 0)}
+var a = make(chan int, 3)
+ 
+func T6() {
+    wg := sync.WaitGroup{}
+    wg.Add(3)
+    go func() {
+        defer wg.Done()
+        a <- 10
+        globalMap["test"] = append(globalMap["test"], <- a)
+    }()
+    go func() {
+        defer wg.Done()
+        a <- 11
+        globalMap["test2"] = append(globalMap["test2"], <- a)
+    }()
+    go func() {
+        defer wg.Done()
+        a <- 12
+        globalMap["test3"] = append(globalMap["test3"], <- a)
+    }()
+
+    wg.Wait()
+	close(a)
+
+    fmt.Printf("%v", globalMap)
+    
+}
+
