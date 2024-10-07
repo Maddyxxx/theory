75abc04a196baa64a05ed12b5ef53b8eceb938e9
Maddyxxx <sevo260@gmail.com>
2024-10-07 20:58:48 +0300

    parallelizm/task_6

--- platform/practice/parallelizm/task_6.go
+++ platform/practice/parallelizm/task_6.go
@@ -41,34 +41,46 @@ func main() {
 // в зависимости от порядка выполнения горутин
 // исправить можно
 
-
+// работа с мапой непотокобезопасна
+// нужен мьютекс который надо внутрь го-функции передавать
+// когда пишем писать в мапу - ждать мьютекс
 
 var globalMap = map[string][]int{"test": make([]int, 0), "test2": make([]int, 0), "test3": make([]int, 0)}
-var a = make(chan int, 3)
- 
+var a = 0
+
 func T6() {
-    wg := sync.WaitGroup{}
-    wg.Add(3)
-    go func() {
-        defer wg.Done()
-        a <- 10
-        globalMap["test"] = append(globalMap["test"], <- a)
-    }()
-    go func() {
-        defer wg.Done()
-        a <- 11
-        globalMap["test2"] = append(globalMap["test2"], <- a)
-    }()
-    go func() {
-        defer wg.Done()
-        a <- 12
-        globalMap["test3"] = append(globalMap["test3"], <- a)
-    }()
+	wg := sync.WaitGroup{}
 
-    wg.Wait()
-	close(a)
+	var mu sync.Mutex
 
-    fmt.Printf("%v", globalMap)
-    
+    go func() {
+		wg.Add(1)
+        defer wg.Done()
+		mu.Lock()
+		defer mu.Unlock()
+        a=10
+        globalMap["test"] = append(globalMap["test"], a)
+    }()
+    go func() {
+		wg.Add(1)
+        defer wg.Done()
+		mu.Lock()
+		defer mu.Unlock()
+        a=11
+        globalMap["test2"] = append(globalMap["test2"], a)
+    }()
+    go func() {
+		wg.Add(1)
+        defer wg.Done()
+		mu.Lock()
+		defer mu.Unlock()
+        a=12
+        globalMap["test3"] = append(globalMap["test3"], a)
+    }()
+	
+	wg.Wait()
+	fmt.Printf("%v", globalMap)
+    fmt.Printf("%d", a)
+
 }
 
